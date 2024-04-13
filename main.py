a31b7ab17228e40c159421149bb69fff4c885538
Luis Diaz <76989444@continental.edu.pe>
2024-04-12 20:27:05 -0500

    Se añadió calculo del MCM

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-class CalculadoraMCD:
+class CalculadoraMCD_MCM:
     def __init__(self, num1, num2):
         self.num1 = num1
         self.num2 = num2
@@ -10,9 +10,14 @@ class CalculadoraMCD:
             a, b = b, a % b
         return a
 
+    def calcular_mcm(self):
+        mcd = self.calcular_mcd()
+        return (self.num1 * self.num2) // mcd
+
 # Ejemplo de uso
 num1 = int(input("Ingresa el primer número: "))
 num2 = int(input("Ingresa el segundo número: "))
 
-calculadora = CalculadoraMCD(num1, num2)
+calculadora = CalculadoraMCD_MCM(num1, num2)
 print("El máximo común divisor de", num1, "y", num2, "es:", calculadora.calcular_mcd())
+print("El mínimo común múltiplo de", num1, "y", num2, "es:", calculadora.calcular_mcm())
